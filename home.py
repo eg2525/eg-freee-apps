e9e8e95fcd228bd5ee5296ca8792f6e58f2ec842
eg2525 <149653970+eg2525@users.noreply.github.com>
2023-11-14 10:41:40 +0900

    Add files via upload

--- home.py
+++ home.py
@@ -2,13 +2,13 @@ import streamlit as st
 import 残高試算表
 import 変換
 
+# 初期状態では何も表示しないようにセッション状態を設定
+if 'current_app' not in st.session_state:
+    st.session_state['current_app'] = None
+
 st.title('EG_R4→freee変換_home')
 
-# セッション状態の初期化
-if 'current_app' not in st.session_state:
-    st.session_state['current_app'] = None  # 現在表示するアプリを保持する変数
-
-# ボタンが押されたときのアクションを定義する
+# ボタンが押されたときに実行する関数
 def show_app1():
     st.session_state['current_app'] = 'app1'
 
@@ -16,13 +16,13 @@ def show_app2():
     st.session_state['current_app'] = 'app2'
 
 # アプリ選択用のボタン
-col1, col2 = st.columns(2)
-with col1:
-    st.button('残高試算表', on_click=show_app1)
-with col2:
-    st.button('仕訳', on_click=show_app2)
+if st.button('残高試算表'):
+    show_app1()
 
-# 選択されたアプリに基づいて表示を変更
+if st.button('仕訳'):
+    show_app2()
+
+# 選択されたアプリを表示
 if st.session_state['current_app'] == 'app1':
     残高試算表.app1()
 elif st.session_state['current_app'] == 'app2':
